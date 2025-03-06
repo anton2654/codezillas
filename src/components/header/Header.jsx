247875105abde02e95975fe4cbe80e1ccf2d6130
Your Name <bohdan.lol66@gmail.com>
2025-03-06 18:11:14 +0200

    Add header functionality.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./header.css";
 import { Refrigerator, Beef, CupSoda, Calculator } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -17,28 +18,42 @@ const Header = () => {
     return (
         <div className={`header-container ${isScrolled ? "scrolled" : ""}`}>
             <div className='header-logo'>
-                <img src="/logo.png" alt="Логотип" className='header-logo-img'/>
+                <NavLink to="/main" style={{textDecoration: 'none'}}>
+                    <img src="/logo.png" alt="Логотип" className='header-logo-img'/>
+                </NavLink>
             </div>
             <nav className='header-nav'>
                 <div className='header-nav-link'>
-                    <Refrigerator size={25} strokeWidth={1.25}/>
-                    <span>Холодильник</span>
+                    <NavLink to="/fridge" className='header-nav-link'>
+                        <Refrigerator size={25} strokeWidth={1.25}/>
+                        <span>Холодильник</span>
+                    </NavLink>
                 </div>
                 <div className='header-nav-link'>
-                    <Beef size={25} strokeWidth={1.25}/>
-                    <span>Страви</span>
+                    <NavLink to="/dishes" className='header-nav-link'>
+                        <Beef size={25} strokeWidth={1.25}/>
+                        <span>Страви</span>
+                    </NavLink>
+
                 </div>
                 <div className='header-nav-link'>
-                    <CupSoda size={25} strokeWidth={1.25}/>
-                    <span>Напої</span>
+                    <NavLink to="/drinks" className='header-nav-link'>
+                        <CupSoda size={25} strokeWidth={1.25}/>
+                        <span>Напої</span>
+                    </NavLink>
                 </div>
                 <div className='header-nav-link'>
-                    <Calculator size={25} strokeWidth={1.25}/>
-                    <span>Калькулятор</span>
+                    <NavLink to="/calculator" className='header-nav-link'>
+                        <Calculator size={25} strokeWidth={1.25}/>
+                        <span>Калькулятор</span>
+                    </NavLink>
                 </div>
             </nav>
             <div className='header-profile'>
-                <div className='header-profile-pic'></div>
+                <NavLink to="/profile" className='header-nav-link'>
+                    <div className='header-profile-pic'></div>
+                </NavLink>
+
             </div>
         </div>
     );
