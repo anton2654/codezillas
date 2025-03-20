56b23bffda89237bb3ec380a4a15a4abc6ed90ba
Valentyn26543 <149696068+Valentyn26543@users.noreply.github.com>
2025-03-20 12:23:21 +0200

    some little fixes about scroll-up

--- frontend/src/components/userButton/userButton.jsx
+++ frontend/src/components/userButton/userButton.jsx
@@ -1,41 +1,47 @@
-import React from 'react';
-import './userButton.css'
-import { User } from 'lucide-react';
-import { LogOut } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./userButton.css";
+import { User } from "lucide-react";
+import { LogOut } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const UserButton = () => {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-    // temp solution for sure
-    const currentUser = true;
+  // temp solution for sure
+  const currentUser = true;
 
-    return currentUser ? (
-        <div className="user-button">
-            <div onClick={() => setOpen((prev) => !(prev))}
-                 className="user-button-img">
-            </div>
+  return currentUser ? (
+    <div className="user-button">
+      <div
+        onClick={() => setOpen((prev) => !prev)}
+        className="user-button-img"
+      ></div>
 
-            {open && (
-                <div className="user-options">
-                    <div>
-                        <Link to="/profile" className={"profile-option"}> Профіль <User size={16} /> </Link>
-                    </div>
-                    <hr></hr>
-                    <div className="log-out-option">Вийти <LogOut size={16} /></div>
-                </div>
-            )}
-        </div>
-    ) : (
-        <div className="auth-buttons">
-            <Link to="/login">
-                <button className="login-button">Увійти</button>
+      {open && (
+        <div className="user-options">
+          <div>
+            <Link to="/profile" className={"profile-option"}>
+              {" "}
+              Профіль <User size={16} />{" "}
             </Link>
-            <Link to="/register">
-                <button className="register-button">Зареєструватися</button>
-            </Link>
+          </div>
+          <hr></hr>
+          <div className="log-out-option">
+            Вийти <LogOut size={16} />
+          </div>
         </div>
-    );
+      )}
+    </div>
+  ) : (
+    <div className="auth-buttons">
+      <Link to="/login">
+        <button className="login-button">Увійти</button>
+      </Link>
+      <Link to="/register">
+        <button className="register-button">Зареєструватися</button>
+      </Link>
+    </div>
+  );
 };
 
 export default UserButton;
