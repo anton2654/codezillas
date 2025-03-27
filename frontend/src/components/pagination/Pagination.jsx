fce092b2429a046027bdddd348ad2c499095cdea
Your Name <bohdan.lol66@gmail.com>
2025-03-27 18:37:55 +0200

    Redesign pagination component.

--- frontend/src/components/pagination/Pagination.jsx
+++ frontend/src/components/pagination/Pagination.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactPaginate from "react-paginate";
 import "./pagination.css";
+import { ChevronLeft, ChevronRight, Ellipsis  } from 'lucide-react';
 
 const Pagination = ({ onChangePage, pageCount }) => {
   return (
-    <ReactPaginate
-      className="root"
-      breakLabel="..."
-      nextLabel=">"
-      previousLabel="<"
-      onPageChange={(event) => onChangePage(event.selected + 1)}
-      pageRangeDisplayed={4}
-      pageCount={pageCount}
-      renderOnZeroPageCount={null}
-    />
+      <ReactPaginate
+          className="root"
+          breakLabel={<Ellipsis size={16} />}
+          nextLabel={<ChevronRight size={16} />}
+          previousLabel={<ChevronLeft size={16} />}
+          onPageChange={(event) => onChangePage(event.selected + 1)}
+          pageRangeDisplayed={4}
+          pageCount={pageCount}
+          renderOnZeroPageCount={null}
+      />
   );
 };
 
